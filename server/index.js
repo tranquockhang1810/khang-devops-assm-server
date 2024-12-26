cd2cdd91b499eb9220d00abd25b8594ac2aa5c0d
Tran Quoc Khang <tranquockhang1810@gmail.com>
2024-12-27 02:48:25 +0700

    Assm submit

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const Product = mongoose.model('Product', {
 });
 
 app.get('/', (req, res) => {
-  res.send('DevOps Assignment!');
+  res.send('Khang DevOps Assignment!');
 });
 
 app.get('/api/v1/products', (req, res) => {
